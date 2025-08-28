c860f8c6d509fac0d0e2305f1e067e609e28cfee
herman katende <hermankats16@gmail.com>
2025-08-28 11:14:42 +0300

    modified programs

--- apps/programs/views.py
+++ apps/programs/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import Program
 
-# Create your views here.
+def program_list(request):
+    programs = Program.objects.all()
+    return render(request, "programs/program_list.html", {"programs": programs})
+
+def add_program(request):
+    if request.method == "POST":
+        name = request.POST["name"]
+        description = request.POST["description"]
+        national_alignment = request.POST["national_alignment"]
+        focus_areas = request.POST["focus_areas"]
+        phases = request.POST["phases"]
+        Program.objects.create(
+            name=name,
+            description=description,
+            national_alignment=national_alignment,
+            focus_areas=focus_areas,
+            phases=phases
+        )
+        return redirect("program_list")
+    return render(request, "programs/add_program.html")
