76693daffc293a38b7610c43444e6b65e208ca19
herman katende <hermankats16@gmail.com>
2025-08-28 11:11:57 +0300

    modified programs

--- apps/programs/models.py
+++ apps/programs/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+class Program(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    national_alignment = models.CharField(max_length=200)
+    focus_areas = models.CharField(max_length=200)
+    phases = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.name
