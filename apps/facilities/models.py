83547890451eebdd0509769aba7452f2f959498d
herman katende <hermankats16@gmail.com>
2025-09-01 03:46:44 +0300

    momdified facility model

--- apps/facilities/models.py
+++ apps/facilities/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+class Facility(models.Model):
+    name = models.CharField(max_length=100)
+    location = models.CharField(max_length=200)
+    description = models.TextField()
+    partner_organization = models.CharField(max_length=200)
+    facility_type = models.CharField(max_length=100)
+    capabilities = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.name
