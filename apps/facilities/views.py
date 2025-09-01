94505dfb555db2c86d3d91d105afe06410c04a83
herman katende <hermankats16@gmail.com>
2025-09-01 03:47:27 +0300

    momdified facility controller

--- apps/facilities/views.py
+++ apps/facilities/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import Facility
 
-# Create your views here.
+def facility_list(request):
+    facilities = Facility.objects.all()
+    return render(request, "facilities/facility_list.html", {"facilities": facilities})
+
+def facility_create(request):
+    if request.method == "POST":
+        Facility.objects.create(
+            name=request.POST["name"],
+            location=request.POST["location"],
+            description=request.POST["description"],
+            partner_organization=request.POST["partner_organization"],
+            facility_type=request.POST["facility_type"],
+            capabilities=request.POST["capabilities"],
+        )
+        return redirect("facility_list")
+    return render(request, "facilities/facility_form.html")
