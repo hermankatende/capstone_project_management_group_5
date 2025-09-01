b0b0510983fed16a09ca656a357d8a9fa6fd24dc
herman katende <hermankats16@gmail.com>
2025-09-01 03:58:13 +0300

    modified facility model

--- apps/facilities/models.py
+++ apps/facilities/models.py
@@ -1,12 +1,13 @@
 from django.db import models
 
 class Facility(models.Model):
-    name = models.CharField(max_length=100)
-    location = models.CharField(max_length=200)
-    description = models.TextField()
-    partner_organization = models.CharField(max_length=200)
-    facility_type = models.CharField(max_length=100)
-    capabilities = models.CharField(max_length=200)
+    name = models.CharField(max_length=120)
+    location = models.CharField(max_length=255, blank=True)
+    description = models.TextField(blank=True)
+    partner_organization = models.CharField(max_length=255, blank=True)
+    facility_type = models.CharField(max_length=100, blank=True)
+    capabilities = models.CharField(max_length=255, blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return self.name
