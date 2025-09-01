51534f406355babb7baff532520e2649d346d260
Otana <emmaodongo10@gmail.com>
2025-09-01 05:07:25 +0300

    modified views

--- apps/projects/views.py
+++ apps/projects/views.py
@@ -1,3 +1,43 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
+from django.contrib import messages
+from .models import Project
+from .forms import ProjectForm
 
-# Create your views here.
+def project_list(request):
+    """Show all projects."""
+    projects = Project.objects.select_related('program', 'facility').order_by('-created_at')
+    return render(request, 'projects/project_list.html', {'projects': projects})
+
+def project_create(request):
+    """Add a new project."""
+    if request.method == 'POST':
+        form = ProjectForm(request.POST)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Project created successfully.')
+            return redirect('project_list')
+    else:
+        form = ProjectForm()
+    return render(request, 'projects/project_form.html', {'form': form})
+
+def project_edit(request, pk):
+    """Edit an existing project."""
+    project = get_object_or_404(Project, pk=pk)
+    if request.method == 'POST':
+        form = ProjectForm(request.POST, instance=project)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Project updated successfully.')
+            return redirect('project_list')
+    else:
+        form = ProjectForm(instance=project)
+    return render(request, 'projects/project_form.html', {'form': form, 'project': project})
+
+def project_delete(request, pk):
+    """Delete a project."""
+    project = get_object_or_404(Project, pk=pk)
+    if request.method == 'POST':
+        project.delete()
+        messages.success(request, 'Project deleted successfully.')
+        return redirect('project_list')
+    return render(request, 'projects/project_confirm_delete.html', {'project': project})
