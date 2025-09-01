59dbf88ffec046f9742a84ff142116617541902c
Saamuel <mcxeirols@gmail.com>
2025-09-01 03:40:32 +0300

    modified program controller

--- apps/programs/views.py
+++ apps/programs/views.py
@@ -5,19 +5,14 @@ def program_list(request):
     programs = Program.objects.all()
     return render(request, "programs/program_list.html", {"programs": programs})
 
-def add_program(request):
+def program_create(request):
     if request.method == "POST":
-        name = request.POST["name"]
-        description = request.POST["description"]
-        national_alignment = request.POST["national_alignment"]
-        focus_areas = request.POST["focus_areas"]
-        phases = request.POST["phases"]
         Program.objects.create(
-            name=name,
-            description=description,
-            national_alignment=national_alignment,
-            focus_areas=focus_areas,
-            phases=phases
+            name=request.POST["name"],
+            description=request.POST["description"],
+            national_alignment=request.POST["national_alignment"],
+            focus_areas=request.POST["focus_areas"],
+            phases=request.POST["phases"],
         )
         return redirect("program_list")
-    return render(request, "programs/add_program.html")
+    return render(request, "programs/program_form.html")
