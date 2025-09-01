99d59117f7eae5d0f5c76fac5717b9da32bea6d6
Saamuel <mcxeirols@gmail.com>
2025-09-01 03:44:27 +0300

    modified program url mapping

--- apps/programs/urls.py
+++ apps/programs/urls.py
@@ -3,5 +3,5 @@ from . import views
 
 urlpatterns = [
     path("", views.program_list, name="program_list"),
-    path("add/", views.add_program, name="add_program"),
+    path("add/", views.program_create, name="program_create"),
 ]
