0396d98af1d8dd4b0028cab359e9c18f21cf6c40
Samuel <mcxeirols@gmail.com>
2025-09-01 05:49:46 +0300

    modified outcome model

--- apps/outcomes/models.py
+++ apps/outcomes/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from projects.models import Project
 
-# Create your models here.
+class Outcome(models.Model):
+    project = models.ForeignKey(Project, on_delete=models.CASCADE, related_name='outcomes')
+    title = models.CharField(max_length=220)
+    description = models.TextField(blank=True)
+    artifact_link = models.URLField(blank=True, null=True)
+    outcome_type = models.CharField(max_length=100, blank=True)
+    quality_certification = models.CharField(max_length=200, blank=True, null=True)
+    commercialization_status = models.CharField(max_length=100, blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
