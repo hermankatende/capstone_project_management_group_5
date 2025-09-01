6ecbae2457ea5f15176877516b119993b93a5948
Otana <emmaodongo10@gmail.com>
2025-09-01 06:41:53 +0300

    making general modifications

--- capstone_project_management/settings.py
+++ capstone_project_management/settings.py
@@ -64,7 +64,7 @@ ROOT_URLCONF = 'capstone_project_management.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+    'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
