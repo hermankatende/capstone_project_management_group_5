bc34bb0f98a4f078596c51dbf4923284fd81de3b
Rania Nabatanzi <ranianabatanzi004@gmail.com>
2025-09-01 05:24:18 +0300

    extended participant to project assignment

--- apps/participants/urls.py
+++ apps/participants/urls.py
@@ -6,4 +6,8 @@ urlpatterns = [
     path('add/', views.participant_create, name='participant_create'),
     path('<int:pk>/edit/', views.participant_edit, name='participant_edit'),
     path('<int:pk>/delete/', views.participant_delete, name='participant_delete'),
+
+    # Project assignments
+    path('assign/<int:project_id>/', views.assign_participant, name='assign_participant'),
+    path('remove/<int:assignment_id>/', views.remove_assignment, name='remove_assignment'),
 ]
