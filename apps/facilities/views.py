5c576d85e7182bdd33034b65005a4f8deff87c9c
herman katende <hermankats16@gmail.com>
2025-09-01 04:00:00 +0300

    modified facility controller

--- apps/facilities/views.py
+++ apps/facilities/views.py
@@ -1,19 +1,36 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, get_object_or_404, redirect
 from .models import Facility
+from .forms import FacilityForm
+from django.contrib import messages
 
 def facility_list(request):
-    facilities = Facility.objects.all()
-    return render(request, "facilities/facility_list.html", {"facilities": facilities})
+    facilities = Facility.objects.order_by('-created_at')
+    return render(request,'facilities/facility_list.html',{'facilities':facilities})
 
 def facility_create(request):
-    if request.method == "POST":
-        Facility.objects.create(
-            name=request.POST["name"],
-            location=request.POST["location"],
-            description=request.POST["description"],
-            partner_organization=request.POST["partner_organization"],
-            facility_type=request.POST["facility_type"],
-            capabilities=request.POST["capabilities"],
-        )
-        return redirect("facility_list")
-    return render(request, "facilities/facility_form.html")
+    if request.method=='POST':
+        form = FacilityForm(request.POST)
+        if form.is_valid():
+            form.save()
+            messages.success(request,'Facility created.')
+            return redirect('facility_list')
+    else:
+        form = FacilityForm()
+    return render(request,'facilities/facility_form.html',{'form':form})
+
+def facility_edit(request, pk):
+    facility = get_object_or_404(Facility, pk=pk)
+    form = FacilityForm(request.POST or None, instance=facility)
+    if request.method=='POST' and form.is_valid():
+        form.save()
+        messages.success(request,'Facility updated.')
+        return redirect('facility_list')
+    return render(request,'facilities/facility_form.html',{'form':form,'facility':facility})
+
+def facility_delete(request, pk):
+    facility = get_object_or_404(Facility, pk=pk)
+    if request.method=='POST':
+        facility.delete()
+        messages.success(request,'Facility deleted.')
+        return redirect('facility_list')
+    return render(request,'facilities/facility_confirm_delete.html',{'facility':facility})
