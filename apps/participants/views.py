dc720a2f6ca6f7224c3a875c21c52f8ad0918e81
Rania Nabatanzi <ranianabatanzi004@gmail.com>
2025-09-01 05:18:14 +0300

    added participant views

--- apps/participants/views.py
+++ apps/participants/views.py
@@ -1,3 +1,30 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+from .models import Participant, ProjectParticipant
+from .forms import ParticipantForm, ProjectParticipantForm
+from django.contrib import messages
 
-# Create your views here.
+def participant_list(request):
+    people = Participant.objects.order_by('-created_at')
+    return render(request,'participants/participant_list.html',{'people':people})
+
+def participant_create(request):
+    if request.method=='POST':
+        form = ParticipantForm(request.POST)
+        if form.is_valid():
+            form.save()
+            messages.success(request,'Participant added.')
+            return redirect('participant_list')
+    else:
+        form = ParticipantForm()
+    return render(request,'participants/participant_form.html',{'form':form})
+
+def assign_to_project(request):
+    if request.method=='POST':
+        form = ProjectParticipantForm(request.POST)
+        if form.is_valid():
+            form.save()
+            messages.success(request,'Assigned participant to project.')
+            return redirect('participant_list')
+    else:
+        form = ProjectParticipantForm()
+    return render(request,'participants/assign_form.html',{'form':form})
